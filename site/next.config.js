eab6240b04ec49936537d3b0c3b417e98960c49a
erigazio <erigazio@gmail.com>
2024-06-12 12:31:15 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'EAR Registry',
+    description: 'Mein test',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://erigazio.github.io/kasm-registry-ear/',
+    contactUrl: 'https://erigazio.github.io/kasm-registry-ear/',
   },
   reactStrictMode: true,
   swcMinify: true,
